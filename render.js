47fab18c9a2973569005a2792d660a9fd8311b1a
Carlton Joseph <carlton.joseph@gmail.com>
2024-09-30 14:47:29 -0700

    add node to js ipc

--- render.js
+++ render.js
@@ -1,2 +1,8 @@
 const information = document.getElementById("info");
 information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`;
+
+const pingBtn = document.getElementById("ping");
+pingBtn.addEventListener("click", async () => {
+  const response = await window.versions.ping();
+  console.log(response);
+});
